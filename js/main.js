0aa3cde212ba67e875ef52e66a4ac170ec2f91d1
turovets90 <a.turovets90@gmail.com>
2018-11-30 20:16:56 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -27,6 +27,84 @@ $(document).ready(function(){
         });
     }
 
+    $('.cat_item').each(function(){
+        var cat_item=$(this);
+        var toggler=$(this).find('.cat_toggle');
+        var cat_submenu=$(this).find('.cat_submenu');
+        $(toggler).on('click', function(){
+            if($(cat_submenu).is(':visible')){
+                $(cat_submenu).slideUp();
+                $(cat_item).removeClass('active');
+            }else{
+                $(cat_submenu).slideDown();
+                $(cat_item).addClass('active');
+            }
+        });
+    });
+
+
+
+    $('.tab-content .tab-pane').each(function(){
+        var tab_content_slider = $(this).find('.tab_content_slider');
+        var tab_content_slider_item = $(this).find('.tab_content_slider > div');
+        if(tab_content_slider_item.length > 4){
+            $(tab_content_slider).slick({
+                autoplay: false,
+                dots: true,
+                arrows: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3
+                        }
+                    },
+                    {
+                        breakpoint: 991,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 575,
+                        settings: {
+                            slidesToShow: 1
+                        }
+                    }
+                ]
+            });
+        }else if($(window).innerWidth() < 575 && tab_content_slider_item.length > 1){
+            $(tab_content_slider).slick({
+                autoplay: false,
+                dots: false,
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            });
+        }else if($(window).innerWidth() < 767 && tab_content_slider_item.length > 2){
+            $(tab_content_slider).slick({
+                autoplay: false,
+                dots: false,
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 1
+            });
+        }else if($(window).innerWidth() < 991 && tab_content_slider_item.length > 3){
+            $(tab_content_slider).slick({
+                autoplay: false,
+                dots: false,
+                arrows: true,
+                slidesToShow: 3,
+                slidesToScroll: 1
+            });
+        }
+
+    });
+
+
+
 
 
     /*
